38699ee762cfe2f663b20e21ac5d105f9370637e
Yuki <future.starshine@gmail.com>
2022-11-07 15:49:13 +0800

    Update test scripts.

--- tests/main.sh
+++ tests/main.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 limit=${1}
 shift 1
+fail=0
 
-cnt=0
-while [ ${cnt} -lt ${limit} ]
+for i in ${@}
 do
-    sleep 5
-    
-    for i in ${@}
+
+    cnt=0
+    while [ ${cnt} -lt ${limit} ]
     do
+        
+        sleep 3
         ./${i}
 
-        if [ ! ${?} -eq 0 ]; then
-            continue
+        if [ ${?} -eq 0 ]; then
+            break
         fi
+
+        cnt=$(( cnt + 1 ))
     done
 
-    if [ ${?} -eq 0 ]; then
-        break
+    if [ ${cnt} -eq ${limit} ]; then
+        fail=1
     fi
-
-    cnt=$(( cnt + 1 ))
 done
+
+if [ ${fail} -ne 0 ]; then
+    exit 1
+fi
